3e4d8ae2d8818f58ca6f62930cc653f63b597e0d
Bruno Caboclo dos Santos Ribeiro <bcaboclo@cpqd.com.br>
2022-07-14 17:42:02 -0300

    Criado TransferAttributesList

---
+++ src/components/TransferAttributesList.jsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import Grid from '@mui/material/Grid';
+import List from '@mui/material/List';
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import Checkbox from '@mui/material/Checkbox';
+import Button from '@mui/material/Button';
+import Divider from '@mui/material/Divider';
+
